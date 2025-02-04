33f93b0bc20934d4259edc5bc8efe582b269b8c3
techking11 <nma1132000@gmail.com>
2025-02-04 11:35:13 +0700

    add images in public

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css';
+import BottomToTop from './components/BottomToTop/BottomToTop';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div>
-      <p className="text-red-600">haha</p>
-    </div>
+    <>
+      <Footer />
+      <BottomToTop />
+    </>
   );
 }
-
 export default App;
