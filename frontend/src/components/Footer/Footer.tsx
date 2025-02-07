37b72b436abd86a2b2d289bd9ace2c3fdc4bb627
techking11 <nma1132000@gmail.com>
2025-02-07 22:12:28 +0700

    component: add Footers

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -4,15 +4,11 @@ import { FaHeart } from 'react-icons/fa6';
 
 export default function Footer() {
   return (
-    <Box className="w-full bg-[#4eb2e2] flex flex-col justify-center items-center text-white text-center font-medium py-5">
-      <img
-        className="size-14 rounded-full shadow shadow-cyan-600"
-        src="/nma.png"
-        alt="nma"
-      />
-      <p className="flex items-center pt-3">
+    <Box className="w-full bg-cyan-600 flex flex-col justify-center items-center text-white text-center font-medium py-5 px-5">
+      <img className="size-14 rounded-full" src="/nma.png" alt="nma" />
+      <p className="pt-3">
         Copyright &copy;{new Date().getFullYear()} All rights reserved &nbsp;
-        <FaHeart /> &nbsp; by Nay Myo Aung
+        <FaHeart className="inline-block" /> &nbsp; by Justin Myo .
       </p>
       <a href="" className="flex items-center pt-1">
         <CiEdit /> &nbsp; <span className="hover:underline">About Me</span>
